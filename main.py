ba164b759bfb61332e07a0479fb4fcb973741121
Youssef <84783747+YoussefYasserYY@users.noreply.github.com>
2023-04-06 13:43:07 +0200

    commit

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ from streamlit_webrtc import webrtc_streamer
 
 st.title("My first Streamlit app")
 
-webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": ["http://localhost:8502/"]}]})
