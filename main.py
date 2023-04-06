41f4391d7b05b41664bef2a65c502079fd37f5f8
Youssef <84783747+YoussefYasserYY@users.noreply.github.com>
2023-04-06 11:56:38 +0200

    commit

--- main.py
+++ main.py
@@ -38,10 +38,18 @@ st.title("Face Detection")
 
 # webrtc_streamer(key="example", video_frame_callback=callback)
 
+import cv2
+import streamlit as st
 
-video_file = open('myvideo.mp4', 'rb')
-video_bytes = video_file.read()
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-st.video(video_bytes)
-
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
 
