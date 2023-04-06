247d42e3f9de5cdfd1892b67ffc13188e197c946
Youssef <84783747+YoussefYasserYY@users.noreply.github.com>
2023-04-06 14:42:14 +0200

    commit

--- main.py
+++ main.py
@@ -66,36 +66,6 @@ import av
 st.title("live stream")
 
 
-
-
-
-# def callback(frame):
-#     img = frame.to_ndarray(format="bgr24")
-
-#     img = cv2.cvtColor(cv2.COLOR_GRAY2BGR)
-
-#     return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-
-# webrtc_streamer(
-#     key="example",
-#     video_frame_callback=callback,
-#     rtc_configuration={  # Add this line
-#         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-#     }
-# )
-
-
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
-import cv2
-
-st.title("My first Streamlit app")
-st.write("Hello, world")
-
-
 class VideoProcessor:
     def __init__(self) -> None:
         self.threshold1 = 100
@@ -104,7 +74,7 @@ class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+        img = cv2.cvtColor(cv2.COLOR_GRAY2BGR)
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
@@ -116,6 +86,3 @@ ctx = webrtc_streamer(
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
