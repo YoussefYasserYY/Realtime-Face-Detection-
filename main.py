ad93dfcb81c4d206b72ac1235bb4f03e9115db4b
Youssef <84783747+YoussefYasserYY@users.noreply.github.com>
2023-05-03 14:55:36 +0300

    commit

--- main.py
+++ main.py
@@ -1,88 +1,40 @@
-# import streamlit as st
-# from streamlit_webrtc import webrtc_streamer
-# import cv2
-# import av
-# import face_recognition
-
-# st.title("Face Detection")
-# Find all the faces in the current frame of video
-
-
-
-
-# def callback(frame):
-#     img = frame.to_ndarray(format="bgr24")
-#     st.write(img.shape)
-
-#     # img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
-#     # img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2))
-#     # rgb_frame = frame[:, :, ::-1]
-#     # Find all the faces in the current frame of video
-#     face_locations = face_recognition.face_locations(frame)
-#     for top, right, bottom, left in face_locations:
-#         img = cv2(cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2))
-#     return av.VideoFrame.from_ndarray(img, format="bgr24")
-# def callback(frame):
-#     img = frame.to_ndarray()
-
-#     img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-#     st.image(frame)
-#     # rgb_frame = frame[:, :, ::-1]
-#     # face_locations = face_recognition.face_locations(rgb_frame)
-    
-#     # for top, right, bottom, left in face_locations:
-#         # img = cv2(cv2.rectangle(rgb_frame, (left, top), (right, bottom), (0, 0, 255), 2))
-    
-#     # x = av.img.VideoFrame(1920, 1080, 'rgb24')
-#     return av.VideoFrame.from_ndarray(img)
-
-# webrtc_streamer(key="example", video_frame_callback=callback)
-
 import cv2
 import streamlit as st
-import acapture
 
-# ret, im = camera.read()
-
-st.title("Video stream")
-run = st.checkbox('start')
-FRAME_WINDOW = st.image([])
-camera = acapture.open(0)
-
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
+# Define a function to detect faces
+def detect_faces(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return img
 
-# import streamlit as st
-# from streamlit_webrtc import webrtc_streamer
-# import cv2 
-# import av
+# Initialize camera capture
+cap = cv2.VideoCapture(0)
 
+# Set output dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# st.title("live stream")
-
-
-# class VideoProcessor:
-#     def __init__(self) -> None:
-#         self.threshold1 = 100
-#         self.threshold2 = 200
-
-#     def recv(self, frame):
-#         img = frame.to_ndarray(format="bgr24")
-
-#         img = cv2.cvtColor(cv2.COLOR_GRAY2BGR)
-
-#         return av.VideoFrame.from_ndarray(img, format="bgr24")
+# Create a Streamlit window to display the output
+st.title("Face Detection App")
+output = st.empty()
 
 
-# ctx = webrtc_streamer(
-#     key="example",
-#     video_processor_factory=VideoProcessor,
-#     rtc_configuration={
-#         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-#     }
+T = st.checkbox('Stop video')
+
+# Main loop to detect and display faces
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = detect_faces(frame)
+    output.image(frame, channels="BGR")
+    if T==True:
+        break
+
+
